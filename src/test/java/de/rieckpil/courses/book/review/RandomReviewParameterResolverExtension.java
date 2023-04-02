fb99ebf9ba4649492646d796b370ba9aaf7b1d3b
Herpai Péter <herpaip@datapress.hu>
2023-04-02 13:32:35 +0200

    Add lorem ipsum review test implementation

--- src/test/java/de/rieckpil/courses/book/review/RandomReviewParameterResolverExtension.java
+++ src/test/java/de/rieckpil/courses/book/review/RandomReviewParameterResolverExtension.java
@@ -15,7 +15,7 @@ import static java.lang.annotation.ElementType.PARAMETER;
 
 public class RandomReviewParameterResolverExtension implements ParameterResolver {
 
-  private static final List<String> badReviews = List.of("This book was shit I don't like it",
+  private static final List<String> badReviews = List.of("This book was shit I don't like it. I hope I will find a better one.",
     "I was reading the book and I think the book is okay. I have read better books and I think I know what's good",
     "Good book with good agenda and good example. I can recommend for everyone");
 
