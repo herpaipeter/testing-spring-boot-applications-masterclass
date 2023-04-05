74f7f877d70e5dff6b301470702e00db4494e361
Herpai Péter <herpaip@datapress.hu>
2023-04-05 15:46:45 +0200

    Add simple DataJpaTest assertions

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
@@ -18,10 +18,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @DataJpaTest(properties = {
   "spring.flyway.enabled=false",
   "spring.jpa.hibernate.ddl-auto=create-drop",
-  "spring.datasource.driver-class-name=com.p6spy.engine.spy.P6SpyDriver", // P6Spy
-  "spring.datasource.url=jdbc:p6spy:h2:mem:testing;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false" // P6Spy
+//  "spring.datasource.driver-class-name=com.p6spy.engine.spy.P6SpyDriver", // P6Spy
+//  "spring.datasource.url=jdbc:p6spy:h2:mem:testing;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false" // P6Spy
 })
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ReviewRepositoryTest {
 
   @Autowired
@@ -38,6 +38,11 @@ class ReviewRepositoryTest {
 
   @Test
   void notNull() throws SQLException {
+    assertNotNull(entityManager);
+    assertNotNull(cut);
+    assertNotNull(dataSource);
+
+    System.out.println(dataSource.getConnection().getMetaData().getDatabaseProductName());
   }
 
   @Test
