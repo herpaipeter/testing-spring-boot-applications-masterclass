fc56017a5a87f506ce46f3aa2156e3460a00be59
Herpai Péter <herpaip@datapress.hu>
2023-04-09 11:06:06 +0200

    Add book contoller valid result test

--- src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookControllerTest.java
@@ -11,7 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -40,10 +43,27 @@ class BookControllerTest {
 
   @Test
   void shouldNotReturnXML() throws Exception {
+    this.mockMvc.perform(get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML))
+        .andExpect(status().isNotAcceptable());
   }
 
   @Test
   void shouldGetBooksWhenServiceReturnsBooks() throws Exception {
+    Book bookOne = createBook(1L, "1234324234343", "Java 11", "Mike", "Good Java book", "Programming", 342L, "Publish Ltd.", "http://localhost/1");
+    Book bookTwo = createBook(2L, "1234324276343", "Java 17", "Duke", "Good Java book", "Programming", 284L, "Publish Ltd.", "http://localhost/2");
+    when(bookManagementService.getAllBooks()).thenReturn(List.of(bookOne, bookTwo));
+    this.mockMvc.perform(get("/api/books")
+        .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+      .andExpect(status().is(200))
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.size()", is(2)))
+      .andExpect(jsonPath("$.[0].id").doesNotExist())
+      .andExpect(jsonPath("$.[0].isbn", is("1234324234343")))
+      .andExpect(jsonPath("$.[0].title", is("Java 11")))
+      .andExpect(jsonPath("$.[1].id").doesNotExist())
+      .andExpect(jsonPath("$.[1].isbn", is("1234324276343")))
+      .andExpect(jsonPath("$.[1].title", is("Java 17")));
   }
 
   private Book createBook(Long id, String isbn, String title, String author, String description, String genre, Long pages, String publisher, String thumbnailUrl) {
