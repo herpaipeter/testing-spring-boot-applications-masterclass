5817994a7d4a4d38b1267244f2b055f817ccefdc
Herpai Péter <herpaip@datapress.hu>
2023-04-12 18:51:01 +0200

    Add review contoller reviews endpoint default value tests

--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerTest.java
@@ -7,19 +7,24 @@ import de.rieckpil.courses.config.WebSecurityConfig;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReviewController.class)
 // see https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.7-Release-Notes#migrating-from-websecurityconfigureradapter-to-securityfilterchain
@@ -34,8 +39,36 @@ class ReviewControllerTest {
 
   private ObjectMapper objectMapper;
 
+  @BeforeEach
+  public void beforeEach() {
+    this.objectMapper = new ObjectMapper();
+  }
+
+  @Test
+  void shouldCallServiceWithDefaultValues() throws Exception {
+    mockMvc.perform(get("/api/books/reviews"));
+    ArgumentCaptor<Integer> firstArgCaptor = ArgumentCaptor.forClass(Integer.class);
+    ArgumentCaptor<String> secondArgCaptor = ArgumentCaptor.forClass(String.class);
+    verify(reviewService).getAllReviews(firstArgCaptor.capture(), secondArgCaptor.capture());
+    assertEquals(20, firstArgCaptor.getValue());
+    assertEquals("none", secondArgCaptor.getValue());
+  }
+
   @Test
   void shouldReturnTwentyReviewsWithoutAnyOrderWhenNoParametersAreSpecified() throws Exception {
+    ArrayNode result = objectMapper.createArrayNode();
+    ObjectNode statistic = objectMapper.createObjectNode();
+    statistic.put("bookId", 1L);
+    statistic.put("isbn", "42");
+    statistic.put("avg", new BigDecimal("3.42"));
+    statistic.put("ratings", 10);
+    result.add(statistic);
+
+    when(reviewService.getAllReviews(20, "none")).thenReturn(result);
+
+    mockMvc.perform(get("/api/books/reviews"))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.size()", is(1)));
   }
 
   @Test
